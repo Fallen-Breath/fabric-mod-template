009d06e6fd13c1aa23a747e2c60f8ecec2d306d7
Fallen_Breath <yqx791125833@gmail.com>
2023-01-08 23:26:02 +0800

    define mod id / name in gradle.properties and reuse them

--- src/main/java/me/fallenbreath/template_mod/TemplateMod.java
+++ src/main/java/me/fallenbreath/template_mod/TemplateMod.java
@@ -22,6 +22,7 @@ package me.fallenbreath.template_mod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.metadata.ModMetadata;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -29,13 +30,15 @@ public class TemplateMod implements ModInitializer
 {
 	public static final Logger LOGGER = LogManager.getLogger();
 
-	public static final String MOD_NAME = "TemplateMod";
 	public static final String MOD_ID = "template_mod";
-	public static String VERSION = "unknown";
+	public static String MOD_VERSION = "unknown";
+	public static String MOD_NAME = "unknown";
 
 	@Override
 	public void onInitialize()
 	{
-		VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(RuntimeException::new).getMetadata().getVersion().getFriendlyString();
+		ModMetadata metadata = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(RuntimeException::new).getMetadata();
+		MOD_NAME = metadata.getName();
+		MOD_VERSION = metadata.getVersion().getFriendlyString();
 	}
 }
